755f3fca507c78a61d5413f423730d82002fb35c
Simon Swain <simon@simonswain.com>
2014-09-04 09:35:48 +1000

    Cleanup

--- tasks/reset.js
+++ tasks/reset.js
@@ -1,5 +1,6 @@
+"use strict";
+
 module.exports = function(grunt) {
- 
   grunt.registerTask('reset', 'Resets database to pristine state', function() {
     var done = this.async();
     var api = require('../api');
