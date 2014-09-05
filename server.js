5a40ad310a8ce5230b310cc04cbedfc1c863cafb
Simon Swain <simon@simonswain.com>
2014-09-05 16:49:12 +1000

    Getting rid of caps

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var os = require('./lib');
 var logger = require( './lib/logger');
 var config = require( './config');
-var server = os.Server(config);
+var server = os.server(config);
 
 server.start(function(){
   logger.log( 'info','Objectstore running on ' + config.host + ':' + config.port );
