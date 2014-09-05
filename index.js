3c5f94a87e06b6fc855591353ee926e040915785
Simon Swain <simon@simonswain.com>
2014-09-05 10:05:44 +1000

    Refactoring

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * objectstore
+ * https://github.com/simonswain/objectstore
+ *
+ * Copyright (c) 2014 Simon Swain
+ * Licensed under the MIT license.
+ */
+
+module.exports = require('./api');
--- server.js
+++ server.js
@@ -1,55 +1,10 @@
-var Hapi = require('hapi');
-var async = require('async');
-
-
-var logger = require('./lib/logger');
-var api = require('./api');
-var db = require('./lib/db');
-
-var server = Hapi.createServer('localhost', 8000);
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply({'objectstore': '0.0.1'});
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/stats',
-  handler: function (request, reply) {
-    api.stats(function(err, res){
-      reply(res);
-    });
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/objects/{id}',
-  handler: function (request, reply) {
-    reply('Not implemented yet');
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/objects',
-  handler: function (request, reply) {
-    api.find({}, function(err, objs){
-      reply(objs);
-    });
-  }
-});
+var config = require( '../config');
+var server = require('./api/server')(config);
+server.start();
 
 process.on( 'SIGINT', function() {
-  logger.log( 'info','Shutting Down...' );
+  console.log( 'info','Shutting Down...' );
   server.stop(function(){
-    db.close();
-    logger.log( 'info','Finished.' );
+    console.log( 'info','Finished.' );
   });
 });
-
-server.start();
