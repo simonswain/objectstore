5d4d0835d423dc3852d639b1dfa75c99d259f884
Simon Swain <simon@simonswain.com>
2014-09-05 16:44:25 +1000

    Added basic (not validating yet) REST routes and tests

--- test/rest_test.js
+++ test/rest_test.js
@@ -15,6 +15,8 @@ var client = require('nodeunit-httpclient').create({
   status: 200
 });
 
+var myObj
+
 exports.rest = {
 
   setUp: function(done) {
@@ -39,15 +41,136 @@ exports.rest = {
     test.expect(4);
     client.get(
       test, 
-      'stats', {
-        status: 200
-      }, function(res) {
+      'stats', function(res) {
         test.equals(typeof res.data, 'object');
         test.ok(res.data.hasOwnProperty('objects'));
         test.ok(res.data.hasOwnProperty('relations'));
         test.done();
       });
   },
+
+  'get-none': function(test) {
+    test.expect(6);
+    client.get(
+      test, 
+      'objects', function(res) {
+        test.equals(typeof res.data, 'object');
+        test.ok(res.data.hasOwnProperty('objects'));
+        test.ok(res.data.hasOwnProperty('count'));
+        test.equals(res.data.count, 0);
+        test.equals(res.data.objects.length, 0);
+        test.done();
+      });
+  },
+
+  'add': function(test) {
+    test.expect(4);
+
+    myObj = {
+      type: 'doc',
+      slug: 'doc-1',
+      attrs: {
+        foo: 'bar'
+      }
+    };
+
+    client.post(
+      test, 
+      'objects', {
+        data: myObj
+      }, function(res) {
+        myObj.id = res.data.id;
+        test.equals(typeof res.data, 'object');
+        test.equal(res.data.type, myObj.type);
+        test.equal(res.data.slug, myObj.slug);
+        test.done();
+      });
+  },
+
+  'get-one': function(test) {
+    test.expect(3);
+    client.get(
+      test,
+      'objects/' + myObj.id, 
+      function(res) {
+        test.equals(typeof res.data, 'object');
+        test.deepEqual(res.data, myObj);
+        test.done();
+      });
+  },
+
+  'set': function(test) {
+    test.expect(2);
+
+    myObj.attrs.foo = 'baz';
+
+    client.put(
+      test, 
+      'objects/' + myObj.id, {
+        data: myObj.attrs
+      }, function(res) {
+        test.deepEqual(res.data.attrs, myObj.attrs);
+        test.done();
+      });
+  },
+
+  'get-set': function(test) {
+    test.expect(2);
+    client.get(
+      test,
+      'objects/' + myObj.id, {
+        status: 200
+      }, function(res) {
+        test.deepEqual(res.data.attrs, myObj.attrs);
+        test.done();
+      });
+  },
+
+  'stats-one': function(test) {
+    test.expect(2);
+    client.get(
+      test, 
+      'stats', 
+      function(res) {
+        test.equals(res.data.objects, 1);
+        test.done();
+      });
+  },
+
+  'del': function(test) {
+    test.expect(1);
+
+    client.del(
+      test, 
+      'objects/' + myObj.id,
+      function(res) {
+        test.done();
+      });
+  },
+
+  'del-done-stats': function(test) {
+    test.expect(2);
+    client.get(
+      test, 
+      'stats', {
+      }, function(res) {
+        test.equals(res.data.objects, 0);
+        test.done();
+      });
+  },
+
+  'del-done-get?': function(test) {
+    test.expect(1);
+    client.get(
+      test,
+      'objects/' + myObj.id, {
+      }, {
+        status: 404
+      }, function(res) {
+        test.done();
+      });
+  },
+
 
   'server-stop': function(test) {
     server.stop(function(){
