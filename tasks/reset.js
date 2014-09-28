e95123577848c4bbf11bdb88b87dc38cf989e932
Simon Swain <simon@simonswain.com>
2014-09-29 09:34:36 +1000

    Fix config usage

--- tasks/reset.js
+++ tasks/reset.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt) {
   grunt.registerTask('reset', 'Resets database to pristine state', function() {
     var done = this.async();
-    var config = require('../config');
+    var config = require('../config')(process.env.NODE_ENV);
     var api = require('../lib').api(config);
     api.reset(done);
   });
